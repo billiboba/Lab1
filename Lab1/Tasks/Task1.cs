add2b0b855b0a7d9c51cf79b3d5b76435b836200
billiboba <romakor.19@yandex.ru>
2024-09-27 18:09:46 +0700

    add some corrections

---
+++ Lab1/Tasks/Task1.cs
@@ -0,0 +1,112 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Lab1
+{
+    public class Task1
+    {
+        public static double Convertor(double degree, string scale, string scale_second)
+        {
+            switch (scale)
+            {
+                case "C":
+                    if (scale == scale_second)
+                    {
+                        return degree;
+                    }
+                    else if (scale_second == "K")
+                    {
+                        return degree + 273.15;
+                    }
+                    else if (scale_second == "F")
+                    {
+                        return degree * 9 / 5 + 32;
+                    }
+                    break;
+                case "K":
+                    if (scale == scale_second)
+                    {
+                        return degree;
+                    }
+                    else if (scale_second == "C")
+                    {
+                        return degree - 273.15;
+                    }
+                    else if (scale_second == "F")
+                    {
+                        return degree * 9 / 5 - 459.67;
+                    }
+                    break;
+                case "F":
+                    if (scale == scale_second)
+                    {
+                        return degree;
+                    }
+                    else if (scale_second == "C")
+                    {
+                        return (degree - 32) * 5 / 9;
+                    }
+                    else if (scale_second == "K")
+                    {
+                        return (degree + 459.67) * 5 / 9;
+                    }
+                    break;
+            }
+            return 0;
+        }
+        public static double GetValidDoubleInput(string message)
+        {
+            double result = 0;
+            bool isValidInput = false;
+
+            while (!isValidInput)
+            {
+                Console.WriteLine(message);
+                string input = Console.ReadLine();
+
+                if (double.TryParse(input, out result))
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: введите числовое значение.");
+                }
+            }
+
+            return result;
+        }
+
+        public static string GetValidScaleInput(string message)
+        {
+            string result = null;
+            bool isValidInput = false;
+
+            while (!isValidInput)
+            {
+                Console.WriteLine(message);
+                result = Console.ReadLine().ToUpper();
+
+                if (Regex.IsMatch(result, @"\d"))
+                {
+                    Console.WriteLine("Ошибка: введите значения C,K,F");
+                    switch(result)
+                    {
+                        case "C":
+                            continue;
+                        case "K":
+                            continue;
+                        case "F":
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    isValidInput = true;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Lab1/Tasks/Task4.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+class Task4
+{
+    public static List<double> ReadCsvFile(string filePath)
+    {
+        List<double> data = new();
+
+        try
+        {
+            using StreamReader reader = new StreamReader(filePath);
+            string line;
+            bool isFirstRow = true;
+            Regex regex = new Regex(@"\d+([.,]\d+)?");
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (isFirstRow)
+                {
+                    isFirstRow = false;
+                    continue;
+                }
+                MatchCollection matches = regex.Matches(line);
+
+                foreach (Match match in matches)
+                {
+                    string value = match.Value.Replace(",", ".");
+
+                    if (double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out double result))
+                    {
+                        data.Add(result);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при чтении файла: {ex.Message}");
+        }
+
+        return data;
+    }
+    public static double CalculateCorrectedVariance(List<double> data)
+    {
+        double mean = data.Average();
+        double sumOfSquares = data.Sum(x => Math.Pow(x - mean, 2));
+        return sumOfSquares / (data.Count - 1);
+    }
+}
